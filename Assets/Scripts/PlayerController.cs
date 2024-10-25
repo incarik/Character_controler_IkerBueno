e06d0b7e08665eaa39673decfb5a0025dc52ba40
incarik <incarik1@gmail.com>
2024-10-25 09:45:29 +0200

    gravedad + movimiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,16 @@ public class NewBehaviourScript : MonoBehaviour
     private float _horizontal;
     private float _vertical;
 
+    //--------------Cosas Gravedad--------------------
 
+    [SerializeField] private float  _gravity = -9.81f;
+    [SerializeField] private Vector3 _playerGravity;
+    
+    //--------------Cosas GroundSensor----------------
+    [SerializeField] Transform _sensorPosition;
+    [SerializeField] float _sensorRadius = 0.5f;
+    [SerializeField] LayerMask _groundLayer;
+    [SerializeField] private bool _isGrounded;
     void Awake()
     {
         _controller = GetComponent<CharacterController>();
@@ -30,11 +39,39 @@ public class NewBehaviourScript : MonoBehaviour
         _vertical = Input.GetAxis("Vertical");
 
         Movement();
+
+        Gravity();
     }
     void Movement()
     {
         Vector3 direction = new Vector3(_horizontal, 0, _vertical);
 
         _controller.Move(direction * _movimentSpeed * Time.deltaTime);
+    }
+
+    void Gravity()
+    {
+        if(!_isGrounded)
+        {
+            _playerGravity.y += _gravity * Time.deltaTime;
+        }
+        else if(_isGrounded && _playerGravity.y < 0)
+        {
+            _playerGravity.y = -1;
+        }
+       
+        _controller.Move(_playerGravity * Time.deltaTime);
+    }
+
+    void IsGrounded()
+    {
+        if(Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer))
+        {
+            _isGrounded = true;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
     }
 }
